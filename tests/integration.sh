168944b62baa704dcfaa7de2fb69fda76dddabe0
Célestin Matte <celestin.matte@gmail.com>
2020-11-13 18:10:56 +0100

    Add tests for all fields

--- tests/integration.sh
+++ tests/integration.sh
@@ -10,7 +10,6 @@ fi
 function run() {
     address=$1
     keyword=$2
-    echo "Testing grepping $keyword on page $address" 
     wget -O - $target/$address 2>/dev/null | grep -F "$keyword" >/dev/null
     if [ $? -ne 0 ]
     then
@@ -18,7 +17,28 @@ function run() {
     fi
 }
 
+echo "Testing vendorlist"
 run "/vendorlist?id=60" "data: [0,"
+
+echo "Testing vendorlist?id"
 run "/vendorlist?id=-2" "Error"
 run "/vendorlist?id=sdklfjs" "Error"
 run "/vendorlist?id=9000" "Error"
+
+echo "Testing vendors"
+run "/vendors?vendorlistid=60&consentpurposeid=8&categ=0" "1020, Inc."
+
+echo "Testing vendors?vendorlistid"
+run "/vendors?vendorlistid=-1&consentpurposeid=8&categ=0" "Error"
+run "/vendors?vendorlistid=sdklfj&consentpurposeid=8&categ=0" "Error"
+run "/vendors?vendorlistid=9000&consentpurposeid=8&categ=0" "Error"
+
+echo "Testing vendors?consentpurposeid"
+run "/vendors?vendorlistid=60&consentpurposeid=-1&categ=0" "Error"
+run "/vendors?vendorlistid=60&consentpurposeid=sdkfl&categ=0" "Error"
+run "/vendors?vendorlistid=60&consentpurposeid=9000&categ=0" "Error"
+
+echo "Testing vendors?categ"
+run "/vendors?vendorlistid=60&consentpurposeid=8&categ=-1" "Error"
+run "/vendors?vendorlistid=60&consentpurposeid=8&categ=sdfkljsd" "Error"
+run "/vendors?vendorlistid=60&consentpurposeid=8&categ=9000" "Error"
