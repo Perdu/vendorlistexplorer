0f5511e3f618f7e97294d9ad7f42946f89cb9a1e
Célestin Matte <celestin.matte@gmail.com>
2020-12-15 18:08:03 +0100

    evolution.py: add a 'relative' option to display results in terms of proportions of vendors

--- evolution.py
+++ evolution.py
@@ -30,9 +30,11 @@ def execute(query, parameters = None, return_rowcount = False, return_rows = Fal
     cur.close()
     return res
 
-def compute_evolution(purpose_number):
+def compute_evolution(purpose_number, relative=False):
     counts = []
     dates = []
+    total_counts = []
+    relative_counts = []
     query = "SELECT COUNT(*), lastUpdated FROM vendor_legint LEFT JOIN vendorlist ON vendor_legint.vendorlist_id = vendorlist.id WHERE legint = %d GROUP BY vendorlist_id \
              UNION SELECT 0, lastUpdated FROM vendorlist WHERE id NOT IN (SELECT vendorlist_id FROM vendor_legint WHERE legint = %d AND vendor_legint.vendorlist_id = vendorlist.id) \
              ORDER BY lastUpdated" % (purpose_number, purpose_number)
@@ -40,13 +42,28 @@ def compute_evolution(purpose_number):
     for row in rows:
         counts.append(row[0])
         dates.append(row[1])
-    plt.plot(dates, counts)
+    if relative:
+        query = "SELECT COUNT(*), vendorlist_id FROM vendor GROUP BY vendorlist_id \
+                 UNION SELECT 0, id FROM vendorlist WHERE id NOT IN (SELECT vendorlist_id FROM vendor WHERE vendor.vendorlist_id = vendorlist.id) \
+                 ORDER BY vendorlist_id"
+        rows = execute(query, return_rows = True)
+        for row in rows:
+            total_counts.append(row[0])
+        for vendorlist_id in range(0, len(counts)):
+            if total_counts[vendorlist_id] > 0:
+                relative_counts.append(counts[vendorlist_id]/total_counts[vendorlist_id])
+            else:
+                relative_counts.append(0)
+        plt.plot(dates, relative_counts)
+        plt.ylabel('Proportion of vendors using purpose %d with leg int' % purpose_number)
+    else:
+        plt.plot(dates, counts)
+        plt.ylabel('Nb vendors using purpose %d with leg int' % purpose_number)
     plt.xlabel("time")
-    plt.ylabel('Nb vendors using purpose %d with leg int' % purpose_number)
     plt.show()
 
 if __name__ == "__main__":
     db = start_db()
     for i in range(1,11):
-        compute_evolution(i)
-    #compute_evolution(4)
+        compute_evolution(i, relative=True)
+    #compute_evolution(4, relative=True)
