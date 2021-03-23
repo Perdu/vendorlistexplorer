5921dee0a2513a6fc0f8d63ccf6bd1e510c70511
Célestin Matte <celestin.matte@gmail.com>
2021-03-23 19:27:19 +0100

    Add delete_vendorlist.sh to delete a vendorlist version from the database

---
+++ delete_vendorlist.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 VENDORLIST_ID"
+    exit
+fi
+
+id=$1
+
+echo "Deleting vendorlist $id"
+
+sudo mysql -u root -v vendorlist_browser -e "SET FOREIGN_KEY_CHECKS=0;\
+delete from vendor_feature where vendorlist_id = $id; \
+delete from vendor_flexible_purpose where vendorlist_id = $id;\
+delete from vendor_legint where vendorlist_id = $id;\
+delete from vendor_purpose where vendorlist_id = $id;\
+delete from vendor_special_feature where vendorlist_id = $id;\
+delete from vendor_special_purpose where vendorlist_id = $id;\
+delete from vendor where vendorlist_id = $id;\
+delete from vendorlist where id = $id;\
+SET FOREIGN_KEY_CHECKS=1;
+"
