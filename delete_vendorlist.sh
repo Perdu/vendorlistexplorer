7cdfdcf2039f1860bb4e7adc25c3b7aec8945061
Célestin Matte <celestin.matte@gmail.com>
2023-12-08 10:13:22 +0100

    Old database name change that was never pushed

--- delete_vendorlist.sh
+++ delete_vendorlist.sh
@@ -10,7 +10,7 @@ id=$1
 
 echo "Deleting vendorlist $id"
 
-sudo mysql -u root -v vendorlist_browser -e "SET FOREIGN_KEY_CHECKS=0;\
+sudo mysql -u root -v iabsite -e "SET FOREIGN_KEY_CHECKS=0;\
 delete from vendor_feature where vendorlist_id = $id; \
 delete from vendor_flexible_purpose where vendorlist_id = $id;\
 delete from vendor_legint where vendorlist_id = $id;\
